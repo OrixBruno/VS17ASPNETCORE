6ddfb428e15184aac13b3fc38f9d8b993993827a
Tibiotas <blade-falcon@hotmail.com>
2017-07-05 14:33:51 -0300

    Commit

--- ConsoleAspNetCore/Program.cs
+++ ConsoleAspNetCore/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAspNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hsello World!");
         }
     }
 }
